2c50b14f23a567559fe115e18d8abba19283eb60
MHTpro <mht.pro1381@gmail.com>
2023-08-29 00:34:58 -0700

    get permission of location

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,12 +11,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  LocationData? userLocation;
+  MapController mapController = MapController();
+
+  @override
+  void initState() {
+    getUserLocation();
+    super.initState();
+  }
+
+  //get permission of location
+  Future<void> getUserLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    userLocation = await location.getLocation();
+    //move to the user location on the map
+    mapController.move(
+      //location of user
+      LatLng(userLocation!.latitude!, userLocation!.longitude!),
+
+      //zoom of map
+      18.0,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       //flutter map
       body: FlutterMap(
+        mapController: mapController,
         options: MapOptions(
           //point of view
           center: const LatLng(35.715298, 51.404343),
